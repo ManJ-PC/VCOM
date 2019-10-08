54daad55340ad69b2145e0c0f17b197916441273
Camolas <manuelcurral@gmail.com>
2019-10-08 22:49:53 +0100

    update exercise 1

--- 1a.py
+++ 1a.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 
 def ex1a():
-    img = cv2.imread("robo.jpg",cv2.IMREAD_COLOR)
-    cv2.imshow('imagem',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+ img = cv2.imread("robo.jpg",cv2.IMREAD_COLOR)
+ cv2.imshow('imagem',img)
+ cv2.waitKey(0)
+ cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+ ex1a()
